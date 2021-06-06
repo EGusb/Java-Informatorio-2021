7e7a63e86c249b870079ba5adce27b764869b411
EGusb <emggw@hotmail.com>
2021-06-06 16:25:52 -0300

    Ejercicio 4 Lvl 1

--- EjerciciosComplementariosLevel1/Lvl1CompEjerc04.java
+++ EjerciciosComplementariosLevel1/Lvl1CompEjerc04.java
@@ -1,5 +1,5 @@
 /**
- * 4.	Realizar un programa que calcule el factorial de un número:
+ * Realizar un programa que calcule el factorial de un número:
  * n! = 1 x 2 x 3 x 4 x 5 x … x (n-1) x n.
  * 
  * Sin hacer uso de librerías.
@@ -12,9 +12,23 @@ System.out.println();
 int num = scanner.nextInt();   // Leo un entero
 scanner.close();
 */
+import java.util.Scanner;
 
 public class Lvl1CompEjerc04 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println();   // Salto de línea para que se lea bien en consola
         
+        System.out.print("Ingrese un número entero --->  ");
+        int num = scanner.nextInt(); // Leo un entero
+        scanner.close();
+
+        int resultado = num;
+        for (int i = num-1; i > 1; i--) {
+            resultado = resultado * i;
+        }
+        System.out.println("El factorial de " + num + " es: " + resultado);
+
+        System.out.println();   // Salto de línea para que se lea bien en consola
     }
 }
