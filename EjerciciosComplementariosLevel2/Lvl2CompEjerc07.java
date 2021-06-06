2e05efbd2da28e6a481df15cb1f53ade21dd3320
EGusb <emggw@hotmail.com>
2021-06-06 15:19:08 -0300

    Primer commit (templates y consignas)

---
+++ EjerciciosComplementariosLevel2/Lvl2CompEjerc07.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ * 
+ */ 
+
+/*
+import java.util.Scanner;
+Scanner scanner = new Scanner(System.in);
+System.out.println();
+int num = scanner.nextInt();   // Leo un entero
+scanner.close();
+*/
+
+public class Lvl2CompEjerc07 {
+    public static void main(String[] args) {
+        
+    }
+}
