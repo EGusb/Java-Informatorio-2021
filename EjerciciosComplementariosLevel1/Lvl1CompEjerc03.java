a994072921c2d9a32436edcfd8ec493affa56503
EGusb <emggw@hotmail.com>
2021-06-06 16:15:37 -0300

    Ejercicio 3 Lvl 1

--- EjerciciosComplementariosLevel1/Lvl1CompEjerc03.java
+++ EjerciciosComplementariosLevel1/Lvl1CompEjerc03.java
@@ -1,18 +1,30 @@
 /**
  * 
  * 
- */ 
+ */
 
 /*
+ * import java.util.Scanner; Scanner scanner = new Scanner(System.in);
+ * System.out.println(); int num = scanner.nextInt(); // Leo un entero
+ * scanner.close();
+ */
 import java.util.Scanner;
-Scanner scanner = new Scanner(System.in);
-System.out.println();
-int num = scanner.nextInt();   // Leo un entero
-scanner.close();
-*/
 
 public class Lvl1CompEjerc03 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println();   // Salto de línea para que se lea bien en consola
         
+        System.out.print("Ingrese un número entero --->  ");
+        int num = scanner.nextInt(); // Leo un entero
+        scanner.close();
+
+        String linea = "";
+        for (int i = 1; i <= num; i++) {
+            linea = linea + i + " ";
+            System.out.println(linea);
+        }
+
+        System.out.println();   // Salto de línea para que se lea bien en consola
     }
 }
