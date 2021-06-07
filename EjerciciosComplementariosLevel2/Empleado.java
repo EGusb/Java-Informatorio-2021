0a0740d0cfd2005dee3b06a35a2f0880f0cbaeae
EGusb <emggw@hotmail.com>
2021-06-07 18:34:06 -0300

    Creo clase empleado p/ Ejercicio 6 Lvl 2

---
+++ EjerciciosComplementariosLevel2/Empleado.java
@@ -0,0 +1,38 @@
+public class Empleado {
+    private String nombre;
+    private int dni;
+    private int horasTrabajadas;
+    private int	valorPorHora;
+
+    public void setNombre(String name) {
+        this.nombre = name;
+    }
+
+    public void setDni(int num) {
+        this.dni = num;
+    }
+
+    public void setHorasTrabajadas(int hours) {
+        this.horasTrabajadas = hours;
+    }
+
+    public void setValorPorHora(int value) {
+        this.valorPorHora = value;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public int getDni() {
+        return this.dni;
+    }
+
+    public int getHorasTrabajadas() {
+        return this.horasTrabajadas;
+    }
+
+    public int getValorPorHora() {
+        return this.valorPorHora;
+    }
+}
